141e0164986fcc1724efbf3dd8ce1d447cdd4119
HorvLala <horvathwebdev@gmail.com>
2023-08-22 18:43:47 +0200

    first commit

---
+++ 03_reviews/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Review from "./Review";
+import { DiDebian } from "react-icons/di";
+//bfdgfdsfdshfhdfshf
+function App() {
+  return (
+    <main className="container">
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </main>
+  );
+}
+
+export default App;
